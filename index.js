9204b55810f1752e6862c2abc7d3bf9453a3152f
Gabriel <laurencio.arkauss@gmail.com>
2020-11-16 19:13:12 -0300

    publish website

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+ 
+app.post('/slack/events', function (req, res) {
+  console.log('>>> req.body',req.body);
+})
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+ 
+app.listen(3000)
