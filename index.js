563999b9a94d749ac8a6afd04f1f73255dd9f3dc
Gabriel <laurencio.arkauss@gmail.com>
2020-11-16 19:25:18 -0300

    add more logs

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = express()
 app.post('/slack/events', function (req, res) {
   console.log('>>> req.params',req.params);
   console.log('>>> req.body',req.body);
+  console.log('>>> req.query',req.query);
+  console.log('>>> req',req);
 })
 
 app.get('/', function (req, res) {
