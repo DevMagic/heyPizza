e7062638436d47efd0a87330fea8e2277b9c8417
laurencioScript <laurencio.arkauss@gmail.com>
2021-02-25 18:53:16 -0300

    refact: restructuring of archives

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const routes = require('./routes');
-const server = express();
+const express = require('express')
+const routes = require('./routes')
+const server = express()
 
-server.use(express.json());
-server.use(express.urlencoded({ extended: false }));
-server.use(routes);
-server.use(express.static('src/public'));
+server.use(express.json())
+server.use(express.urlencoded({ extended: false }))
+server.use(routes)
+server.use(express.static('src/public'))
 
-module.exports = server;
+module.exports = server
