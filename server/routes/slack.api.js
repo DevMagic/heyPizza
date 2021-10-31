e9cbff25e8542b0849748d2cfad593dae3e075cd
laurencioScript <laurencio.arkauss@gmail.com>
2021-10-31 13:22:14 -0300

    refactor(slack.api): isolating the code that handles slack events

--- server/routes/slack.api.js
+++ server/routes/slack.api.js
@@ -1,76 +1,31 @@
-const Slack = require('slack');
 const { Router } = require('express');
 const routes = Router();
-const serviceFeedback = require('./../services/feedback.service');
 const CHANNEL_ID = 'CQNEAJSF4';
-const userService = require('./../services/user.service');
 const slackService = require('./../services/slack.service');
 
 routes.post('/verification', function (req, res) {
   return   res.status(200).send(req.body.challenge);
 })
   
-
 routes.post('/webhooks', async (request, response) => {
   try {
     console.log('>>> request', request.body);
+    
+    const typeEvent = request.body.type;
 
-    if(request.body.type == 'url_verification'){
-      console.log('challenge', request.body.challenge);
+    if(typeEvent == 'url_verification'){    
       return response.status(200).send(request.body.challenge);
     }
 
     const event = request.body.event;
-    
-    switch (event.type) {
-      case 'member_joined_channel':
 
-        if(CHANNEL_ID != event.channel){
-          return response.status(300).send('channel invalid');
-        }
-        const externalId = event.user;
-        const token = process.env.SLACK_BOT_TOKEN;
-        let params = { user: externalId , token: token };
-        let data = await Slack.users.info(params);
-        let { profile } = data.user;
-
-        const users = await userService.getUserByExternalId(externalId);
-
-        if(!users.length){
-          const newUser = {
-            name: profile.real_name,
-            externalId: externalId,
-            profileImageUrl: profile.image_original,
-          }
-
-          console.log('>>> newUser', newUser);
-          await slackService.sendMessageToSlack({thread: null, channelId : CHANNEL_ID , message : 'Seja bem-vindo a bordo :rocket:'});
-          await userService.create(newUser);
-        }
-
-
-        break;
-      case 'app_mention':
-      
-        if(CHANNEL_ID != event.channel){
-          return response.status(300).send('channel invalid');
-        }
-
-        const feedbackEvent = {
-          createdAt: event.ts,
-          text: event.text,
-          user_external_id: event.user,          
-        }
-
-        await serviceFeedback.newFeedbackBySlackEvent(feedbackEvent);
-        
-        break;
-    
-      default:
-        break;
+    if(CHANNEL_ID != event.channel){
+      return response.status(300).send('channel invalid');
     }
+    
+    await slackService.webhook(event);
 
-    return response.status(200).send(request.body);
+    return response.status(200).send();
 
   }
   catch(e){
--- server/services/slack.service.js
+++ server/services/slack.service.js
@@ -1,4 +1,50 @@
 const Slack = require('slack');
+const CHANNEL_ID = 'CQNEAJSF4';
+const userService = require('./user.service');
+const feedbackService = require('./feedback.service');
+
+module.exports.webhook = async (event) => {
+  switch (event.type) {
+    case 'member_joined_channel':
+      
+      const externalId = event.user;
+      const token = process.env.SLACK_BOT_TOKEN;
+      let params = { user: externalId , token: token };
+      let data = await Slack.users.info(params);
+      let { profile } = data.user;
+
+      const users = await userService.getUserByExternalId(externalId);
+
+      if(!users.length){
+        const newUser = {
+          name: profile.real_name,
+          externalId: externalId,
+          profileImageUrl: profile.image_original,
+        }
+
+        console.log('>>> newUser', newUser);
+        await module.exports.sendMessageToSlack({thread: null, channelId : CHANNEL_ID , message : 'Seja bem-vindo a bordo :rocket:'});
+        await userService.create(newUser);
+      }
+
+
+      break;
+    case 'app_mention':
+
+      const feedbackEvent = {
+        createdAt: event.ts,
+        text: event.text,
+        user_external_id: event.user,          
+      }
+
+      await feedbackService.newFeedbackBySlackEvent(feedbackEvent);
+      
+      break;
+  
+    default:
+      break;
+  }
+}
 
 module.exports.sendMessageToSlack = async ({
   thread, 
